5e5300c724b1ad8b02093a439dec04506d297fe5
olivierperin <opustenn@gmail.com>
2024-05-20 19:25:54 +0200

    eee

--- pages/panier.php
+++ pages/panier.php
@@ -10,8 +10,59 @@
   <section class="lignescomm">
 
     <div class="lignecomm">
+
+      <img src="./assets/images/tomate.jpg"/>
+      
+      <div class="nometprixpanier">
+          <div class="nomlegpanier">Tomates
+          </div>
+          <div class="prixlegpanier">10€
+          </div>
+          </div>
+
+      <div class="qteplusmoinspanier">1
+      </div>
+
+      <div class="plusmoinspanier">
+          <div class="moinspanier">-
+          </div>
+          <div class="pluspanier">+
+          </div>
+      </div>  
+
+      <div class="totalprixpanier">  
+          <div class="totalpanier">Total
+          </div>
+          <div class="prixpanier">10€
+          </div>
+      </div>
+   
+      <span class="cancel">x
+      </span>
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
 
+
+
+
+
+
+
+
   </section>
-  
+  <section class="resume">
+  </section> 
 </div>  
